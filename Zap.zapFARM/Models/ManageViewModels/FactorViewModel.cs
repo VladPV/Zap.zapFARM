6e4e90035ea0ef09beeb307fdccb503d06913f91
VladPV <vlad@zap-t.com>
2017-03-14 11:22:51 +0200

    Add project files.

---
+++ Zap.zapFARM/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zap.zapFARM.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
